2c44073de3acb352974e0d5bd201501d3786d968
yoichiro kawai <acchonburike.416@gmail.com>
2021-04-20 20:40:53 +0900

    update:投稿機能と一覧機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:content))
+    if @post.save
+      redirect_to posts_path, notice: "投稿されました。"
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
 end
